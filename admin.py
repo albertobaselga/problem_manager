fcda5ed07c5a23c98a205ceafd42411b7d3fa2ec
Alberto Baselga <alberto.baselga@gmail.com>
2014-02-26 22:16:41 +0100

    canges

--- admin.py
+++ admin.py
@@ -2,10 +2,10 @@ from models import *
 from django.contrib import admin
 
 class problemAdmin(admin.ModelAdmin):
-	list_display = ('problem_id','priority','description','status','related_tickets')
-	list_filter = ['problem_id','priority','description','status','related_tickets']
-	search_fields = ['description']
-	list_editable = ['description','status']
+	list_display = ('problem_id','priority','description','status','related_tickets','current_owner','affected_components','currect_impact','user_experience','root_cause','workaround','workaround_status','long_term_solution','long_term_solution_status','comments')
+	list_filter = ['problem_id','priority','status','related_tickets','current_owner','affected_components']
+	search_fields = ['description','workaround','long_term_solution','comments']
+	list_editable = ['priority','description','status','related_tickets','current_owner','affected_components','currect_impact','user_experience','root_cause','workaround','workaround_status','long_term_solution','long_term_solution_status','comments']
 	ordering = ['problem_id']
 
 admin.site.register(problem,problemAdmin)
