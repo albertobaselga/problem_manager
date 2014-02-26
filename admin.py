112c937876737124866a620cbf9b3ee0f1a49aad
Alberto Baselga (DEV) <abf@tid.es>
2014-02-26 22:30:34 +0100

    updated admin view

--- admin.py
+++ admin.py
@@ -8,4 +8,10 @@ class problemAdmin(admin.ModelAdmin):
 	list_editable = ['priority','description','status','related_tickets','current_owner','affected_components','currect_impact','user_experience','root_cause','workaround','workaround_status','long_term_solution','long_term_solution_status','comments']
 	ordering = ['problem_id']
 
+	formfield_overrides = {
+    	models.TextField: {'widget': Textarea(
+       		attrs={'rows': 1,
+                'cols': 50})},
+	}
+
 admin.site.register(problem,problemAdmin)
